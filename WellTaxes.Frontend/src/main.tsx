98f05db36d7127f8e7753379bc928fbb139068d6
YaroslavBugayov <bugayov576@gmail.com>
2026-02-24 20:41:59 +0200

    feature: Add alias for src imports

--- WellTaxes.Frontend/src/main.tsx
+++ WellTaxes.Frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import './index.css';
+import '@/index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
 
-import App from './App.tsx';
+import App from '@/app/App.tsx';
 
 registerSW({
     immediate: true,
--- WellTaxes.Frontend/vite.config.ts
+++ WellTaxes.Frontend/vite.config.ts
@@ -1,7 +1,13 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
     server: {
@@ -13,6 +19,12 @@ export default defineConfig({
         port: 4173,
         host: true,
     },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    base: '/',
     plugins: [
         react(),
         tailwindcss(),
