88bea6396aed2bbbb635e3083192cc0172eb9196
kovol <lordoleksiy@gmail.com>
2026-02-27 00:07:05 +0200

    feature: remove gateway

---
+++ WellTaxes.Backend/WellTaxes.Service.Orders/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Identity.Web;
+
+namespace WellTaxes.Service.Orders.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddAuth(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication().AddMicrosoftIdentityWebApi(configuration.GetSection("AzureAd"));
+        }
+    }
+}
