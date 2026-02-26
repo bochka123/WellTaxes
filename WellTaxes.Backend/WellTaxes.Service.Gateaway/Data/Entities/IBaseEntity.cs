2d45b62293f8bcb6633a65a9030441f54479106d
kovol <lordoleksiy@gmail.com>
2026-02-27 00:07:52 +0200

    remove gateway

--- WellTaxes.Backend/WellTaxes.Service.Gateaway/Data/Entities/IBaseEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace WellTaxes.Service.Gateaway.Data.Entities
-{
-    public interface IBaseEntity
-    {
-        public Guid Id { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-    }
-}
