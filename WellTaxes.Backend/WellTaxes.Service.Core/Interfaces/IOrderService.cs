ab8456c111543eda7b4a2c3b6c823d595d327217
Oleksandr Tkach <sasha2003tkach001100@gmail.com>
2026-02-28 01:18:19 +0200

    added correct structure

--- WellTaxes.Backend/WellTaxes.Service.Core/Interfaces/IOrderService.cs
+++ WellTaxes.Backend/WellTaxes.Service.Core/Interfaces/IOrderService.cs
@@ -1,10 +1,10 @@
 using WellTaxes.Service.Core.Entities;
 
-namespace WellTaxes.Service.Core.Services
+namespace WellTaxes.Service.Core.Interfaces
 {
     public interface IOrderService
     {
-        Task<Order> CreateOrderAsync(decimal amount, decimal latitude, decimal longitude);
+        Task<Order> CreateOrderAsync(decimal amount, decimal latitude, decimal longitude, DateTime orderTimestamp);
         Task<Order?> GetOrderByIdAsync(Guid id);
         Task<bool> UpdateOrderAsync(Guid id, decimal amount, decimal latitude, decimal longitude);
         Task<bool> DeleteOrderAsync(Guid id);
